6650e8e40afe1c0e21004fa044e1bd1ffae17484
SebastianBlandon <blandonodz@gmail.com>
2023-12-17 23:54:33 -0500

    feat: me falta definir el tecnico y sale

--- app.js
+++ app.js
@@ -8,13 +8,17 @@ const MockAdapter = require('@bot-whatsapp/database/mock')
 const { handlerAI } = require("./utils");
 const { ttsElevenLabs } = require("./services/eventlab");
 const { initAssistant, retrieveAssistant, sendToAssistant, ttsOpenAI, dalleAPI } = require("./services/openai");
-const { listClients, listTickets, searchByPhoneNumber, searchByIDNumber } = require("./services/wisphub");
+const { listClients, listTickets, searchByPhoneNumber, searchByIDNumber, createTicket } = require("./services/wisphub");
+
 
 let assistant = null;
 let thread = null;
 let clients = null;
+let client = null;
+let idServicio = null;
 let tickets = null;
-let createTicket = false;
+let createNewTicket = false;
+let processCreateTicket = false;
 const path = require('path');
 let data = null;
 
@@ -26,6 +30,7 @@ function getSession(userIdentifier) {
         sessions[userIdentifier] = {
             thread: null,
             assistant: null,
+            client: null,
             isLoggedIn: false,
             // ... any other session-specific data
         };
@@ -36,36 +41,80 @@ function getSession(userIdentifier) {
 const flowWelcome = addKeyword(EVENTS.WELCOME).addAction(
   async (ctx, ctxFn) => {
       try {
+          console.log("Mensaje entrante : ", ctx.body);
           const userIdentifier = ctx.from;
           const session = getSession(userIdentifier);
 
-          if (createTicket) {
-            const searchClient = searchByIDNumber(clients, ctx.body);
-            if (searchClient != null) {
-              console.log("Cliente encontrado: ", searchClient);
+          if (processCreateTicket) {
+            subject = ctx.body;
+            console.log("Proceso de creacion de ticket....", subject);
+            createTicket(session.client.id_servicio, subject);
+            processCreateTicket = false;
+          }
+          else if (createNewTicket) {
+            if (session.client != null) {
               tickets = await listTickets();
+              console.log("tickets : ", tickets, " \ncliente : ", session.client);
               if (tickets != null) {
-                console.log("Tickets encontrados: ", tickets);
+                // Suponiendo que 'response' contiene la respuesta que recibiste
+                idServicio = session.client.id_servicio; // Aquí debes reemplazar con el id_servicio que tienes
+
+                // Verificar si hay algún ticket asociado al id_servicio en la lista de resultados
+                const ticketEncontrado = tickets.results.find(ticket => ticket.servicio.id_servicio === idServicio);
+
+                if (ticketEncontrado) {
+                  // Si se encontró un ticket para el id_servicio específico
+                  console.log('Se encontró un ticket para el servicio:', ticketEncontrado);
+                } else {
+                  // Si no se encontró ningún ticket para el id_servicio específico
+                  processCreateTicket = true;
+                  ctx.body += " Nota del sistema : se procede a crear el ticket al usuario, pidele la descripcion de la falla.";
+                  console.log('No se encontró ningún ticket para este servicio.');
+                }
               }
               else {
                 console.log("No se encontraron tickets");
               }
-              //const ticket = await createTicket(searchClient.id, ctx.body);
-              console.log("Ticket creado: ");
             }
             else {
-              ctx.body += " Nota del sistema : No se encontró el cliente, la cédula no está registrada en el sistema.";
+              client = searchByIDNumber(clients, ctx.body);
+              if (client != null) {
+                session.client = client;
+                tickets = await listTickets();
+                if (tickets != null) {
+                  // Suponiendo que 'response' contiene la respuesta que recibiste
+                  idServicio = session.client.id_servicio; // Aquí debes reemplazar con el id_servicio que tienes
+
+                  // Verificar si hay algún ticket asociado al id_servicio en la lista de resultados
+                  const ticketEncontrado = tickets.results.find(ticket => ticket.servicio.id_servicio === idServicio);
+
+                  if (ticketEncontrado) {
+                    // Si se encontró un ticket para el id_servicio específico
+                    console.log('Se encontró un ticket para el servicio:', ticketEncontrado);
+                  } else {
+                    // Si no se encontró ningún ticket para el id_servicio específico
+                    processCreateTicket = true;
+                    ctx.body += " Nota del sistema : se procede a crear el ticket al usuario, pidele la descripcion de la falla.";
+                    console.log('No se encontró ningún ticket para este servicio.');
+                  }
+                }
+                else {
+                  console.log("No se encontraron tickets");
+                }
+              }
+              else {
+                ctx.body += " Nota del sistema : No se encontró el cliente, la cédula no está registrada en el sistema o la escribieron mal, pidele que vuelvan a escribirla.";
+              }
             }
-            createTicket = false;
+            createNewTicket = false;
           }
-          console.log('Incoming message: ', ctx.body, ' desde num:', userIdentifier);
           if (!session.isLoggedIn) {
             assistant = await retrieveAssistant();
             thread = await initAssistant();
             clients = await listClients();
-            const searchClient = searchByPhoneNumber(clients, ctx.from.substring(2));
-            if (searchClient != null) {
-              data = await sendToAssistant(thread, assistant, ctx.body, searchClient.nombre);
+            client = searchByPhoneNumber(clients, ctx.from.substring(2));
+            if (client != null) {
+              data = await sendToAssistant(thread, assistant, ctx.body, client.nombre);
             }
             else {
               data = await sendToAssistant(thread, assistant, ctx.body, null);
@@ -73,16 +122,20 @@ const flowWelcome = addKeyword(EVENTS.WELCOME).addAction(
             session.isLoggedIn = true; // Update the isLoggedIn flag in the session
             session.thread = thread;
             session.assistant = assistant;
+            session.client = client;
           }
           else {
-            const { thread, assistant } = session;
-            data = await sendToAssistant(thread, assistant, ctx.body, null);
+            data = await sendToAssistant(session.thread, session.assistant, ctx.body, null);
+          }
+          if ((data.includes("ticket") || data.includes("Ticket") || data.includes("soporte") || data.includes("Soporte")) && (data.includes("cédula") || data.includes("Cédula"))) {
+            createNewTicket = true;
+            if (session.client != null) {
+              ctx.body += " Nota del sistema : Ya se tiene la cedula del cliente se procede a crear el ticket al usuario, saludas al clienta y pidele la descripcion de la falla.";
+              data = await sendToAssistant(session.thread, session.assistant, ctx.body, null);
+              processCreateTicket = true;
+            }
           }
           console.log('Mensaje saliente : ', data)
-          if ((data.includes("ticket") || data.includes("Ticket")) && (data.includes("cédula") || data.includes("Cédula"))) {
-            createTicket = true;
-            console.log("Creando ticket....");
-          }
           await ctxFn.flowDynamic(data);
           console.log("🙉 Envio de mensajes completado....");
       } catch (error) {
--- services/wisphub.js
+++ services/wisphub.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const FormData = require('form-data');
 
 const clientsUrl = 'https://api.wisphub.net/api/clientes/';
 const ticketsUrl = 'https://api.wisphub.net/api/tickets/';
@@ -46,6 +47,72 @@ const listTickets = async () => {
     }
 }
 
+const createTicket = async (idService, subject) => {
+    try {
+      const currentDate = new Date(); // Fecha y hora actual
+      const futureDate = new Date();
+      futureDate.setDate(currentDate.getDate() + 10); // Sumar 10 días
+
+      // Formatear las fechas según el formato 'DD/MM/YYYY HH:mm'
+        const formattedCurrentDate = `${
+            currentDate.getDate().toString().padStart(2, '0')
+        }/${
+            (currentDate.getMonth() + 1).toString().padStart(2, '0')
+        }/${
+            currentDate.getFullYear()
+        } ${
+            currentDate.getHours().toString().padStart(2, '0')
+        }:${
+            currentDate.getMinutes().toString().padStart(2, '0')
+        }`;
+        
+        const formattedFutureDate = `${
+            futureDate.getDate().toString().padStart(2, '0')
+        }/${
+            (futureDate.getMonth() + 1).toString().padStart(2, '0')
+        }/${
+            futureDate.getFullYear()
+        } ${
+            futureDate.getHours().toString().padStart(2, '0')
+        }:${
+            futureDate.getMinutes().toString().padStart(2, '0')
+        }`;
+  
+      
+      const data = new FormData();
+      data.append('asuntos_default', 'Internet Lento');
+      data.append('asunto', subject);
+      data.append('tecnico', '0');
+      data.append('descripcion', '<p>El ticket fue creado por el bot de whatsapp, preguntar exactamente la razón del reporte.</p>');
+      data.append('estado', '1');
+      data.append('prioridad', '1');
+      data.append('servicio', idService);
+      data.append('fecha_inicio', formattedCurrentDate);
+      data.append('fecha_final', formattedFutureDate);
+      data.append('origen_reporte', 'redes_sociales');
+      data.append('departamento', 'Soporte Técnico');
+      data.append('departamentos_default', 'Soporte Técnico');
+      data.append('email_tecnico', 'ai_bot_wp@sunnova.com');
+  
+      const config = {
+        method: 'post',
+        url: ticketsUrl,
+        headers: {
+          'Authorization': `Api-Key ${WISPHUB_NET_KEY}`,
+          ...data.getHeaders(),
+        },
+        data: data,
+      };
+  
+      const response = await axios(config);
+      console.log('Ticket creado:', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error al crear el ticket:', error);
+      throw error;
+    }
+  };
+
 // Function to search by phone number
 const searchByPhoneNumber = (clientsObj, phoneNumber) => {
     const foundClient = Object.values(clientsObj.results).find(
@@ -62,4 +129,4 @@ const searchByIDNumber = (clientsObj, IDNumber) => {
     return foundClient || null; // Return the found client or null if not found
 };
 
-module.exports = { listClients, listTickets, searchByPhoneNumber, searchByIDNumber };
+module.exports = { listClients, listTickets, searchByPhoneNumber, searchByIDNumber, createTicket };
